deb5912550e3783c3e722ee721b5f8cf4c42012b
kadir polaktol <kpolatkol5@gmail.com>
2022-05-22 23:17:44 +0300

    model dosyasındaki __init__ silinmişti tekrar eklendi

---
+++ blue_tv_clone/sayfalar/models/__init__.py
@@ -0,0 +1,9 @@
+from sayfalar.models.dublaj import Dublaj
+from sayfalar.models.film_kategorileri import Film_kategorileri
+from sayfalar.models.film_slider import Film_slider
+from sayfalar.models.filmler import Filmler
+from sayfalar.models.oyuncular import Oyuncular
+from sayfalar.models.yonetmenler import Yonetmenler
+
+
+
